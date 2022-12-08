abf72371d7ec842054edf6fd3beb2a02372ed1b1
Induwara Rajapaksha <realrajapaksha@gmail.com>
2022-12-08 19:57:17 +0530

    init

---
+++ api/flight.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+router.get("/", async (req, res) => {
+    try {
+        res.json({
+            status: 200,
+            message: "Get data succesfull",
+        });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send("server error");
+    }
+})
+
+module.exports = router;
