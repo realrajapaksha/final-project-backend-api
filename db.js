707d96941c1f72a9b5cc8f5016bee5a01875c799
Induwara Rajapaksha <realrajapaksha@gmail.com>
2022-12-25 21:14:30 +0530

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const config = require('./config')
 const servicefile = require('./service-account-file.json')
 
 initializeApp({
-    credential: cert(config.FfirebaseOnVercal)
+    credential: cert(servicefile)
 });
 
 const firestore = getFirestore();
